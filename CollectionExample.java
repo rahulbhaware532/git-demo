1e852605141c4ec9aeb9e184b03d09be7f5a27c8
Rahul <rahulbhaware532@gmail.com>
2024-01-30 20:49:35 +0530

    hellow word

--- CollectionExample.java
+++ CollectionExample.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class CollectionExample {
+public class CollectionExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
 }
