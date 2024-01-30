bced8afe934fd276e1a0c2fc4938925ba693bc21
Rahul <rahulbhaware532@gmail.com>
2024-01-30 21:01:24 +0530

    NA

--- CollectionExample.java
+++ CollectionExample.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CollectionExample {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
+
+        List<Integer> list = Arrays.asList(15, 15, 15, 13, 15, 12, 430);
+        List<Integer> collect = list.stream().filter(x -> !x.equals(15)).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
